f2d7375f36462faac30b5b550575fbef4921cc77
DeadMansChest <everyonewins@live.ca>
2015-01-29 12:52:10 -0500

    Credits change

--- lib/Lua/RealWeaponModNames.lua
+++ lib/Lua/RealWeaponModNames.lua
@@ -1,2 +1,13 @@
---v1.4
+--[[
+
+==[ Real Weapon Names ]==
+v1.4.1
+==CREDITS==
+*Code base thanks to hejoro
+*imfdb for most of the weapon IDs
+*Payday Wikia  for the name of the Leupold/Theia Scope
+*/k/-anon and Steam friends for other unidentified attachments and weapons I haven't found or were not ID'd by the two above sources
+
+]]
+
 RegisterScript("lib/Lua/RealWeaponModNames/realnames.lua", 2, "lib/managers/localizationmanager")
